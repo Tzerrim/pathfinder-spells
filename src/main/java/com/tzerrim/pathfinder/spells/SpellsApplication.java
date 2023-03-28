a5fd44ccdf0db6a2e55d5409e192f62b0b2f9d01
Annox Rill <shadowsirius@gmail.com>
2023-03-28 22:37:12 +0400

    Initial

---
+++ src/main/java/com/tzerrim/pathfinder/spells/SpellsApplication.java
@@ -0,0 +1,13 @@
+package com.tzerrim.pathfinder.spells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tzerrim/pathfinder/spells/SpellsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzerrim.pathfinder.spells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
